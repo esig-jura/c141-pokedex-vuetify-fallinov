8e4d7d20b9901a3507ef78b376c2c9bebb60d932
Fallet Steve <steve.fallet@divtec.ch>
2025-04-16 09:52:05 +0200

    ajout cols pour index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,6 +9,20 @@
     <h1 class="mb-6 text-center">
       Pokédex
     </h1>
+
+    <v-row>
+      <v-col
+        v-for="i in 20"
+        cols="12"
+        lg="3"
+        md="4"
+        mx="12"
+        sm="6"
+        xxl="2"
+      >
+        Mon pokémon - {{ i }}
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
